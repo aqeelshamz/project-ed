299f065acd58cd064aaa623624fabf312170a354
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-03-02 17:30:47 +0530

    changes ui

--- src/Pages/Playground/Playground.jsx
+++ src/Pages/Playground/Playground.jsx
@@ -3,8 +3,18 @@ import "./style.css"
 
 export default function PlayGround() {
     return (
-      <h1 className="text-3xl font-bold underline">
-       Welcome to Playground
-      </h1>
+      <div className='main-play'> 
+        <div className='content-area'>
+          <div className='img-part' > 
+            <img src='https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'></img>
+          </div>
+          <div className='text-part'>
+            <h1></h1>
+            <p></p>
+          </div>
+        </div>
+
+      </div>
     )
   }
