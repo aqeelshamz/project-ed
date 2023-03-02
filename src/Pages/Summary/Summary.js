71db677888774daf71be4131cfcf23fe14e1bfb3
Aqeel <aqeelten@gmail.com>
2023-03-02 20:13:57 +0530

    summary page style

--- src/Pages/Summary/Summary.js
+++ src/Pages/Summary/Summary.js
@@ -1,10 +1,67 @@
 import React from "react";
-
+import "./Summary.css";
 
 function Summary() {
-  return <div className="summary-page">
-    <p id="title">Summary</p>
-  </div>;
+  return (
+    <div className="summary-page">
+      <p id="title">📖 Summary</p>
+      <p
+        style={{
+          background: "white",
+          borderRadius: "10px",
+          padding: "15px",
+          width: "80%",
+          fontSize: "1rem",
+        }}
+      >
+        Human life expectancy is an important measure of the overall health and
+        wellness of a society. As of the current knowledge cutoff in 2021, the
+        average life expectancy in the United States is 78 years. This number
+        has been steadily increasing over the past century due to advances in
+        healthcare, sanitation, and lifestyle changes. However, there are still
+        disparities in life expectancy based on factors such as race, income,
+        and access to healthcare.
+        <br />
+        <br />
+        Dwight D. Eisenhower was a prominent political figure in the
+        mid-twentieth century and served as the 34th President of the United
+        States from 1953 to 1961. In 1955, Eisenhower was still in the midst of
+        his presidency, and the United States was navigating the complexities of
+        the Cold War with the Soviet Union. Eisenhower was a member of the
+        Republican Party, and his administration prioritized economic growth and
+        national security. He also championed civil rights and desegregation,
+        although his actions in this area were often criticized as insufficient
+        by civil rights activists.
+        <br />
+        <br />
+        Telescopes are powerful tools used to observe the universe and study
+        celestial objects such as planets, stars, and galaxies. They work by
+        using lenses or mirrors to gather and focus light, allowing astronomers
+        to see objects that would be too dim or far away to observe with the
+        naked eye. Telescopes have played a crucial role in advancing our
+        understanding of the universe and have led to many groundbreaking
+        discoveries over the years.
+        <br />
+        <br />
+        The 1992 Summer Olympics were held in Barcelona, Spain, and marked a
+        significant moment in the history of the Olympic Games. The event was
+        the first Olympics to be held since the fall of the Berlin Wall and the
+        end of the Cold War, and it was an opportunity for countries around the
+        world to come together in a spirit of unity and collaboration. The
+        Barcelona Olympics also saw the debut of several new events, including
+        women's judo and badminton, and featured many memorable moments, such as
+        the Dream Team's dominant performance in men's basketball and the iconic
+        lighting of the Olympic flame by archer Antonio Rebollo.
+        <br />
+        <br />
+        In summary, the above phrases touch on a diverse range of topics,
+        including human health, politics, science, and sports. These snippets of
+        information provide a glimpse into some of the key events and ideas that
+        have shaped our world over the past century, highlighting the ongoing
+        evolution of society and the continued quest for progress and knowledge.
+      </p>
+    </div>
+  );
 }
 
 export default Summary;
