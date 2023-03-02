706ecb4d43c2a34cee23512e26e5b83a04df81f9
Aqeel <aqeelten@gmail.com>
2023-03-02 16:48:39 +0530

    playground

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
-import logo from "./logo.svg"
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./Pages/Home"
-import PlayGround from "./Pages/Playground/Playground"
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import PlayGround from "./Pages/Playground/Playground";
 
 function App() {
   return (
@@ -11,19 +10,11 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route
-            exact
-            path="/PlayGround"
-            element={
-              <h1 className="text-6xl font-bold underline">
-                Hello world!
-              </h1>
-            }
-          />
+          <Route exact path="/PlayGround" element={PlayGround} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
