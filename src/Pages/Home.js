a62c7b0d90b2d629baa4e25526b9c25e42be8af0
Aqeel <aqeelten@gmail.com>
2023-03-02 16:46:42 +0530

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Home.css"
 
 function Home() {
   return (
-    <div>
-      Helloo gooys!!
+    <div className="main">
+      <p id="title">Dypres</p>
+      <p id="subtitle">Dynamic Education</p>
       <br />
       <button onClick={() => (window.location.href = "/PlayGround")}>
         Go to PlayGround ⚡
