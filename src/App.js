e780b62e0e309354f94cc9b1927023ea90ae44b5
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-03-02 16:46:04 +0530

    added tailwind updated playground

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg"
 import "./App.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Pages/Home"
+import PlayGround from "./Pages/Playground/Playground"
 
 function App() {
   return (
