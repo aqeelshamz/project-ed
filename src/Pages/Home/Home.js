1d5718045c9621e882bac50087406bb66b77a01d
Aqeel <aqeelten@gmail.com>
2023-03-02 19:59:36 +0530

    update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,18 +8,14 @@ function Home() {
   return (
     <div className="main">
       <p id="title">Project Ed</p>
-<<<<<<< HEAD
-      <p id="subtitle">Dynamic Education</p>
-=======
       <p id="subtitle">Realtime Speech Educational Presentation</p>
->>>>>>> 0e8fdeec7016fda0a236ef1f98b10fd4b9eb3336
       <br />
       <p
         style={{ fontSize: "1.2rem", fontWeight: "500", marginBottom: "10px" }}
       >
         🎓 Audience Proficiency level:
       </p>
-      <div className="row" style={{marginBottom: "50px"}}>
+      <div className="row" style={{ marginBottom: "50px" }}>
         {proficiencyLevels.map((item, index) => {
           return (
             <button
@@ -37,7 +33,7 @@ function Home() {
       </div>
       <button
         id="button"
-        onClick={() => (window.location.href = "/PlayGround?level=" + proLevel )}
+        onClick={() => (window.location.href = "/PlayGround?level=" + proLevel)}
       >
         Go to PlayGround ⚡
       </button>
