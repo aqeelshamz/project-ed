c8cfeb723d5c69bf3facd3e2a42d5578b7ac5e3b
Aqeel <aqeelten@gmail.com>
2023-03-02 17:36:01 +0530

    proficiency level in layground

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,40 @@
-import React from "react";
-import "./Home.css"
+import React, { useState } from "react";
+import { proficiencyLevels } from "../../utils/utils";
+import "./Home.css";
 
 function Home() {
+  const [proLevel, setProLevel] = useState(0);
+
   return (
     <div className="main">
       <p id="title">Dypres</p>
       <p id="subtitle">Dynamic Education</p>
       <br />
-      <button id="button" onClick={() => (window.location.href = "/PlayGround")}>
+      <p
+        style={{ fontSize: "1.2rem", fontWeight: "500", marginBottom: "10px" }}
+      >
+        🎓 Audience Proficiency level:
+      </p>
+      <div className="row" style={{marginBottom: "50px"}}>
+        {proficiencyLevels.map((item, index) => {
+          return (
+            <button
+              id="pro-btn"
+              style={{
+                background: proLevel === index ? "dodgerblue" : "white",
+                color: proLevel === index ? "white" : "black",
+              }}
+              onClick={() => setProLevel(index)}
+            >
+              {item}
+            </button>
+          );
+        })}
+      </div>
+      <button
+        id="button"
+        onClick={() => (window.location.href = "/PlayGround?level=" + proLevel )}
+      >
         Go to PlayGround ⚡
       </button>
     </div>
--- src/Pages/Playground/Playground.jsx
+++ src/Pages/Playground/Playground.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { proficiencyLevels } from "../../utils/utils";
+import "./style.css";
 
 export default function PlayGround() {
-    return (
-      <div className='main-play'> 
-        <div className='content-area'>
-          <div className='img-part' > 
-            <img src='https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'></img>
-            <img src='https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'></img>
-          </div>
-          <div className='text-part'>
-            <h1></h1>
-            <p></p>
-          </div>
-        </div>
+  const [searchParams, setSearchParams] = useSearchParams();
 
+  return (
+    <div className="main-play">
+      <p>Proficiency level: {proficiencyLevels[searchParams.get("level")]}</p>
+      <div className="content-area">
+        <div className="img-part">
+          <img src="https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"></img>
+          <img src="https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"></img>
+        </div>
+        <div className="text-part">
+          <h1></h1>
+          <p></p>
+        </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
