b3092192f49814f046d81ddbca1244cbf735df51
Aqeel <aqeelten@gmail.com>
2023-03-02 16:49:39 +0530

    playground

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/PlayGround" element={PlayGround} />
+          <Route exact path="/PlayGround" element={<PlayGround/>} />
         </Routes>
       </Router>
     </>
